f8f5d4dd68e11e0b225df415e36bc06cb3a7c8fe
bugintroducer69 <masc1001@stud.ek.dk>
2026-03-07 10:25:42 +0100

    Movie-related features + frontend.
    * Added CRUD and filtering in MovieRepository.
    * Added a (not done) frontend for these (index, filter_search).
    * Added REST-controller endpoints for movie-related functionality.
    * Updated pom.xml to allow for testing.
    * New docker container for development (controlled by ENV file, so make your own AND DO NOT UPLOAD TO GITHUB)

--- src/main/java/com/xp/Repository/MovieRepository.java
+++ src/main/java/com/xp/Repository/MovieRepository.java
@@ -3,20 +3,56 @@ package com.xp.Repository;
 import com.xp.Model.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface MovieRepository extends JpaRepository <Movie, Long> {
-    Optional<Movie> findByMovieTitleContainingIgnoreCase(String movieTitle);
-    @Query("""
-            SELECT m AS movie
-            FROM Movie m
-            LEFT JOIN Show s ON s.movie = m
-            LEFT JOIN Reservation r ON r.show = s
-            LEFT JOIN r.movieTickets mt
-            GROUP BY m
-            ORDER BY COALESCE(COUNT(mt), 0) DESC
-            """) // Whew.
-    List<Movie> findAllByOrderByPopularityDesc(); // Popularity = Tickets sold
+    List<Movie> findAllByMovieTitleContainingIgnoreCase(String movieTitle);
+
+    @Query(value = """
+            SELECT m.*
+            FROM Movies m
+            LEFT JOIN (
+            	SELECT m.movie_id, COUNT(mt.movie_ticket_id) AS tickets_sold
+            	FROM Movies m
+            	LEFT JOIN Shows s ON s.movie_id = m.movie_id
+            	LEFT JOIN Reservations r ON r.show_id = s.show_id
+            	LEFT JOIN MovieTickets mt ON mt.movie_ticket_id = r.ticket_id
+            	GROUP BY m.movie_id
+            ) t ON t.movie_id = m.movie_id
+            ORDER BY COALESCE(t.tickets_sold, 0) DESC;
+            """, nativeQuery = true) // Whew.
+    List<Movie> findAllOrderByPopularityDesc(); // Popularity = Tickets sold
+
+    @Query(value = """
+           SELECT m.*
+           FROM Movies m
+           JOIN (
+                SELECT s.movie_id, MIN(s.start_time) AS next_start_time
+                FROM Shows s
+                JOIN Theaters th ON th.theater_id = s.theater_id
+                LEFT JOIN (
+                    SELECT mt.show_id, COUNT(*) AS sold_tickets
+                    FROM MovieTickets mt
+                    GROUP BY mt.show_id
+                ) sold ON sold.show_id = s.show_id
+                WHERE s.start_time > NOW()
+                AND (COALESCE(th.number_of_rows,0) * COALESCE(th.seats_per_row,0)) > COALESCE(sold.sold_tickets,0)
+                GROUP BY s.movie_id
+           ) avail ON avail.movie_id = m.movie_id
+           ORDER BY TIMESTAMPDIFF(MINUTE, NOW(), avail.next_start_time) ASC;
+           """, nativeQuery = true)
+    List<Movie> findAllOrderByAvailableSeatsShownSoon();
+
+    @Query(value = """
+            SELECT m.*
+            FROM Movies m
+            WHERE m.movie_category IN (:categories)
+            GROUP BY m.movie_id;
+            """, nativeQuery = true)
+    List<Movie> findAllMatchingCategoryList(@Param("categories") List<String> categories);
+    // Finds all movies which fit one of the categories passed by the user.
+
+    List<Movie> findAllByAgeLimitLessThanEqual(Integer ageLimit);
 }
